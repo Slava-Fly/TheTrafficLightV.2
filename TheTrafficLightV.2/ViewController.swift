23b6fe770f263a90c32b508b17530cb8415cdd6a
@Slava-Fly <school.94@bk.ru>
2022-10-24 04:06:48 +0800

    Model of the Traffic Light

--- TheTrafficLightV.2/ViewController.swift
+++ TheTrafficLightV.2/ViewController.swift
@@ -14,9 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var labelSwitchTheLight: UIButton!
     
+    var alphaReal = 0.2
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         redView.frame = CGRect(x: 130, y: 100, width: 150, height: 150)
         redView.layer.cornerRadius = redView.frame.size.width / 2
@@ -31,9 +33,26 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
+    //var myView: UIView? = nil
 
     @IBAction func changeColorOfTraffic() {
         
+        labelSwitchTheLight.setTitle("NEXT", for: .normal)
+        
+        if greenView.alpha == yellowView.alpha {
+            greenView.alpha = 1
+            redView.alpha = alphaReal
+        }
+        else if greenView.alpha > alphaReal && yellowView.alpha == redView.alpha {
+            yellowView.alpha = 1
+            greenView.alpha = alphaReal
+        }
+        else if yellowView.alpha > alphaReal && redView.alpha == greenView.alpha {
+            redView.alpha = 1
+            yellowView.alpha = alphaReal
+        }
     }
     
     
