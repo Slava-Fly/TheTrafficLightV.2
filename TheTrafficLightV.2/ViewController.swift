a0e7db0bf4971b12e95c7d72eb9f66b605b9c9b7
@Slava-Fly <school.94@bk.ru>
2022-10-23 18:52:52 +0800

    Interface of The Traffic Light

--- TheTrafficLightV.2/ViewController.swift
+++ TheTrafficLightV.2/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var labelSwitchTheLight: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.frame = CGRect(x: 130, y: 100, width: 150, height: 150)
+        redView.layer.cornerRadius = redView.frame.size.width / 2
+        
+        yellowView.frame = CGRect(x: 130, y: 300, width: 150, height: 150)
+        yellowView.layer.cornerRadius = yellowView.frame.size.width / 2
+        
+        greenView.frame = CGRect(x: 130, y: 500, width: 150, height: 150)
+        greenView.layer.cornerRadius = greenView.frame.size.width / 2
+        
+        labelSwitchTheLight.layer.cornerRadius = 15
+        
+        
     }
 
-
+    @IBAction func changeColorOfTraffic() {
+        
+    }
+    
+    
 }
 
